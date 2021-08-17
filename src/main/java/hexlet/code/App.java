d6fc56fc253f940e1ca88759a989f213e48ee9b3
Roman <ramzesnic@gmail.com>
2021-08-17 12:53:42 +0300

    add format & options

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -2,10 +2,24 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
-@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
-  description = "Compares two configuration files and shows a difference.")
+@Command(name = "gendiff",
+    mixinStandardHelpOptions = true,
+    version = "gendiff 1.0",
+    description = "Compares two configuration files and shows a difference.")
 public class App {
+  @Parameters(index = "0", paramLabel = "filePath1", description = "path to first file")
+  private String filePath1;
+  @Parameters(index = "1", paramLabel = "filePath2", description = "path to second file")
+  private String filePath2;
+  @Option(names = {"-f", "--format"},
+      paramLabel = "format",
+      description = "output format [default: ${DEFAULT-VALUE}]",
+      defaultValue = "stylish")
+  private String format;
+
   public static void main(String[] args) {
     int exitCode = new CommandLine(new App()).execute(args);
     System.exit(exitCode);
