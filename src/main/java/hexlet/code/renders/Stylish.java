6eb17db0c27e181ca9175c4695dbd5893877abf0
Roman <ramzesnic@gmail.com>
2021-08-24 17:12:26 +0300

    fix

--- src/main/java/hexlet/code/renders/Stylish.java
+++ src/main/java/hexlet/code/renders/Stylish.java
@@ -15,24 +15,27 @@ public final class Stylish implements RenderInterface {
   public String render(Map<String, Map<String, Object>> diff) {
     final List<String> result = new ArrayList<>();
     final String tab = " ";
-    final int repeatTab = 3;
+    final int unchangerTabSize = 4;
+    final int defaultTabSize = 2;
+    final String unchangerTab = tab.repeat(unchangerTabSize);
+    final String defaultTab = tab.repeat(defaultTabSize);
     final Set<String> keys = Utils.getDicKeys(diff);
     final Consumer<String> renderDiff = key -> {
       final Map<String, Object> node = diff.get(key);
       final String nodeType = (String) node.get("type");
       switch (nodeType) {
         case "added" :
-          result.add(tab + "+ " + key + ": " + node.get("after"));
+          result.add(defaultTab + "+ " + key + ": " + node.get("after"));
           break;
         case "deleted" :
-          result.add(tab + "- " + key + ": " + node.get("before"));
+          result.add(defaultTab + "- " + key + ": " + node.get("before"));
           break;
         case "changed" :
-          result.add(tab + "- " + key + ": " + node.get("before"));
-          result.add(tab + "+ " + key + ": " + node.get("after"));
+          result.add(defaultTab + "- " + key + ": " + node.get("before"));
+          result.add(defaultTab + "+ " + key + ": " + node.get("after"));
           break;
         case "unchanged" :
-          result.add(tab.repeat(repeatTab) + key + ": " + node.get("before"));
+          result.add(unchangerTab + key + ": " + node.get("before"));
           break;
         default :
           break;
